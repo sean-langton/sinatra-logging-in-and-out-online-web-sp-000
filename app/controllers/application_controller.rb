3262895fa9457089df28fb484a301c4d33afc796
Sean Langton <slangton1@gmail.com>
2019-02-02 04:11:40 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
+    @user = Helpers.current_user(session)
     erb :account if Helpers.is_logged_in?(session)
     erb :error
   end
